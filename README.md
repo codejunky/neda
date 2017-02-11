84bd9e044b75703569996c5bb945377581fae059
Oussama Bouguerne <oussama.bouguerne@metaworks.xyz>
2017-02-11 18:18:26 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+## Quickstart
+
+To run the app type the following commands
+```
+  git clone git@github.com:codejunky/neda.git
+  npm install
+  npm start
+```
+You also need to install MongoDB and start it prior to starting the app.
---
+++ client/modules/App/App.js
@@ -0,0 +1,61 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+// Import Style
+import styles from './App.css';
+
+// Import Components
+import Helmet from 'react-helmet';
+import DevTools from './components/DevTools';
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isMounted: false };
+  }
+
+  componentDidMount() {
+    this.setState({isMounted: true}); // eslint-disable-line
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.isMounted && !window.devToolsExtension && process.env.NODE_ENV === 'development' && <DevTools />}
+        <div>
+          <Helmet
+            title="Home - NEDA App"
+            titleTemplate="%s - NEDA App"
+            meta={[
+              { charset: 'utf-8' },
+              {
+                'http-equiv': 'X-UA-Compatible',
+                content: 'IE=edge',
+              },
+              {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1',
+              },
+            ]}
+          />
+          <div className={styles.container}>
+            {this.props.children}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.object.isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+// Retrieve data from store as props
+function mapStateToProps(store) {
+  return {
+  };
+}
+
+export default connect(mapStateToProps)(App);
